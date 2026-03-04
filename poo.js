8e623b52895fb73d39e6bba3207c8c8a8281ad3d
ema22nf-afk <ema22nf@gmail.com>
2026-03-04 13:57:53 -0500

    nueva clase combos

--- poo.js
+++ poo.js
@@ -27,3 +27,29 @@ const pizzaHawaina = new Pizza("delgada", "mediana", ["jamon", "piña", "queso"]
 console.log(pizzaHawaina.preparar());
 console.log(pizzaHawaina.hornear());
 console.log(pizzaHawaina.empacar());
+
+class combos extends Pizza {
+   constructor(combo , cantidad, tamano, masa, ingredientes, bebida, postre) {
+      super(masa, tamano, ingredientes);
+      thius.combo = combo;
+      this.cantidad = cantidad;
+      this.bebida = bebida;
+      this.postre = postre;
+   }
+    elegirCombo() {
+      this.preparar();
+      this.hornear();
+      this.empacar();
+
+      console.log(
+       `Has elegido el combo ${this.combo} que incluye ${this.cantidad} pizzas de tamaño ${this.tamano} con masa de 
+       ${this.masa} y los siguientes ingredientes: ${this.ingredientes}. Ademas, incluye una bebida de ${this.bebida}
+       y un postre de ${this.postre}.`
+      );
+      return this;
+   };
+};
+  
+
+const camboFamiliar = new combos("Familiar", 4, "grande", "delgada", ("jamon", "piña", "queso"), "refresco", "helado");
+console.log(comboFamiliar.elegirCombo());

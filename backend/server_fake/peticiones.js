960511a5ea63d6092b941755eb78c9295019b41e
ema22nf-afk <ema22nf@gmail.com>
2026-04-08 15:27:03 -0500

    taller

---
+++ backend/server_fake/peticiones.js
@@ -0,0 +1,198 @@
+var settings = {
+  "url": "http://localhost:3000/button_head/1",
+  "method": "GET",
+  "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "url": "http://localhost:3000/button_head/2",
+  "method": "PUT",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "id": 2,
+    "first_name": "Esra",
+    "last_name": "Howlings",
+    "email": "ehowlings1@yellowpages.com",
+    "gender": "male",
+    "ip_address": "Mazda"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "url": "http://localhost:3000/button_head/4",
+  "method": "PUT",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "id": "4",
+    "first_name": "Jae",
+    "last_name": "Palfrey",
+    "email": "jpalfrey3@hc360.com",
+    "gender": "Female",
+    "ip_address": "porsche"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "url": "http://localhost:3000/button_head/5",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "id": "5",
+    "first_name": "Isidoro",
+    "last_name": "Davies",
+    "email": "idavies4@360.cn",
+    "gender": "Male",
+    "ip_address": "Mercedes-Benz"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "url": "http://localhost:3000/button_head/3",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "id": "3",
+    "first_name": "pepe",
+    "apellido": "Johnstone",
+    "correo electrónico": "sjohnstone2@pagesperso-orange.fr",
+    "género": "Femenino",
+    "dirección_ip": "ford"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "url": "http://localhost:3000/button_head/6",
+  "method": "GET",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "id": "6",
+    "first_name": "godofredo",
+    "last_name": "Sesons",
+    "email": "osesons5@foxnews.com",
+    "gender": "Female",
+    "ip_address": "mustang"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "url": "http://localhost:3000/button_head/7",
+  "method": "PUT",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "id": "7",
+    "first_name": "Sandias",
+    "last_name": "Smoth",
+    "email": "ssmoth6@trellian.com",
+    "gender": "Female",
+    "ip_address": "bicicleta"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "url": "http://localhost:3000/button_head/8",
+  "method": "DELETE",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "id": "8",
+    "first_name": "Westleigh",
+    "last_name": "Quainton",
+    "email": "wquainton7@scribd.com",
+    "gender": "Male",
+    "ip_address": "Mercedes-Benz"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "url": "http://localhost:3000/button_head/9",
+  "method": "DELETE",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "id": "9",
+    "first_name": "Jozef",
+    "last_name": "Giovanardi",
+    "email": "jgiovanardi8@amazonaws.com",
+    "gender": "female",
+    "ip_address": "motorcicle"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "url": "http://localhost:3000/button_head/7",
+  "method": "PUT",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "id": "7",
+    "first_name": "Sandias",
+    "last_name": "Smoth",
+    "email": "ssmoth6@trellian.com",
+    "gender": "mati gei",
+    "ip_address": "bicicleta"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
--- backend/server_fake/readme.md
+++ backend/server_fake/readme.md
@@ -5,3 +5,13 @@
 5: ¿cuando hacer una peticion de tipo patch?
 6: ¿que es soap, cuales son las diferencias con rest?
 7: ¿que es graphQl, cuales son las diferencias con rest?
+
+                            Solucion
+1: Dataset
+2:significa codigo de estado http, indica que la peticion funciono, normalmente aparece arriba de la respuesta
+3:normalmente se hace una peticion http a tu API y revisas la respuesta
+4:comunes; 200 ok, 201 created, 204 no content
+existentes:de redireccion, de error del cliente, de error del servidor
+5:se usa cuando solo cambias uno o pocos atributos
+6:soap es un protocolo para intercambiar informacion, la diferencia es que una sirve como medio de intercambio y la otra como forma de creacion
+7:GraphQL es un lenguaje de consultas para apis, la diferencia rest es simple y tradicional, graphql es flexible, moderno y eficiente                        
